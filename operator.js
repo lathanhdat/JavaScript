17fd14e3db72c08fe0a2362c8233dc225e3cc6e4
dat <datla@gmail.com>
2018-12-01 12:53:44 +0700

    Completed lesson 10

---
+++ operator.js
@@ -0,0 +1,17 @@
+var now,yearJohn,yearMark;
+now = 2018;
+yearJohn = now - 28;
+yearMark = now - 33;
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now /10);
+console.log(typeof now);
+
+var isGreater = yearJohn >= yearMark;
+console.log(isGreater);
+
+var x = 10;
+x *= 2;
+console.log(x);
+x++;
+console.log(x);
---
+++ variable.js
@@ -0,0 +1,27 @@
+var firstName = 'John'; //Make it more morden
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+var isMarried = false;
+//Joining string
+console.log(lastName + '' +age);
+console.log('Is '+ firstName + ' married? ' + isMarried);
+alert('Hellooo');
+
+var fullAge = true ;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var _3year;
+var $3year;
+
+//Input a value:
+var lastofName = prompt('What is your last name?');
+console.log(firstName + '' + lastofName);
+ 
