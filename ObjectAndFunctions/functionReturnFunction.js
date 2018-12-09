2c6cc0afac6c1f665304e5f7c0cd5d35620b854c
dat <datla@gmail.com>
2018-12-09 07:15:04 +0700

    Finish Ex66

---
+++ ObjectAndFunctions/functionReturnFunction.js
@@ -0,0 +1,30 @@
+function interviewQuestion(job)
+{
+    if (job === 'designer')
+    {
+        return function(name)
+        {
+            console.log(name + ', pls explain what UX design is?');
+        }
+    }
+    else if (job === 'teacher')
+    {
+        return function(name)
+        {
+            console.log(name + ', what subject do you teach?');
+        }
+    }
+    else
+    {
+        return function(name)
+        {
+            console.log('Hello ' + name + ' what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('John');
+
+var designerQuestion = interviewQuestion('designer');
+designerQuestion('Jane');
