f87241d1ad92e6b1feb6ee00f903a6c1ae720d7d
Dat <dat.la@terralogic.com>
2019-03-13 17:10:28 +0700

    update

--- learnyounode/test.js
+++ learnyounode/test.js
@@ -3,5 +3,5 @@ console.log(process.execPath);
 
 
 var arrays = ['hello','hi','what'];
-arrays.filter(filename => filename === 'hi');
-arrays.forEach(filedfile => console.log(filedfile));
+arrays.filter(filename => filename === 'hi')
+    .forEach(filedfile => console.log(filedfile));
