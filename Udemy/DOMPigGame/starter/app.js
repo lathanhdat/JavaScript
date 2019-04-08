b63069300bbe725d2f73588052c062dfa2d14bc4
Dat <dat.la@terralogic.com>
2019-04-08 09:34:03 +0700

    update

--- Udemy/DOMPigGame/starter/app.js
+++ Udemy/DOMPigGame/starter/app.js
@@ -8,13 +8,92 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores,  roundScore, activePlayer, dice;
+/*Using querySelector
+Setter way
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>' //HTML code
 
-scores = [0,0]
-roundScore = 0;
-activePlayer = 0;
+Getter way
+var x = document.querySelector('#current-' + activePlayer).textContent;
+console.log(x);
+*/
 
-dice = Math.floor((Math.random() * 6)) + 1; //Create a random number between 1 and 6;
-document.querySelector('#score-0').textContent = dice;
+/*We can use getElementById if we have ID instade of using querySelector*/
 
- 
+var scores, roundScore, activePlayer, gamePlaying;
+
+const initGame = ()=>{
+  scores = [0,0]
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+
+  document.querySelector('.dice').style.display = 'none'; // Use '.' to select class display is property and none is value
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = "Player1";
+  document.getElementById('name-1').textContent = "Player2";
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
+const nextPlayer = ()=>{
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+};
+
+document.querySelector('.btn-roll').addEventListener('click',()=>{
+  if(gamePlaying === true){
+    let lastdice;
+    let dice = Math.floor((Math.random() * 6)) + 1; //Create a random number between 1 and 6;
+    let diceDoom = document.querySelector('.dice')
+    diceDoom.style.display = 'block';
+    diceDoom.src = 'dice-'+dice +'.png';
+
+    if(lastdice == 6 && dice == 6)
+    {
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+      nextPlayer();
+    }
+    if(dice !== 1){
+      roundScore += dice;
+      lastdice = dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+    else{
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click',()=>{
+  if(gamePlaying === true){
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    if(scores[activePlayer] >= 20){
+      gamePlaying = false;
+      document.getElementById('name-' + activePlayer).textContent = "Winner";
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }
+    else{
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector('.btn-new').addEventListener('click',initGame);
+initGame();
